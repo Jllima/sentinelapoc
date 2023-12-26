6eab20daf91d75d0d7f3b4f4f9389c36b72158a1
Jorge <jlimajorge@gmail.com>
2023-12-26 13:09:45 -0300

    turbo stream

--- app/models/denounce.rb
+++ app/models/denounce.rb
@@ -1,3 +1,4 @@
 class Denounce < ApplicationRecord
   has_many :messages
+  broadcasts
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :denounce
+  broadcasts_to :denounce
 end
