e3d501c6172d8df6d6649cc5d1112a83c9afb3cd
Jorge <jlimajorge@gmail.com>
2023-12-28 12:20:15 -0300

    chore: config tailwind

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   get 'messages/new'
   get 'messages/create'
   resources :denounces do
     resources :messages
   end
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
